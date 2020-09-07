195fba22e493e4777a4ec792555fb8b3901016ed
nznmvs <nznmvs@gmail.com>
2020-09-07 15:16:24 +0300

    Updates

---
+++ precache-manifest.ac05ec6a23de6aefff7e29581db99e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2134a0f167aae55c68ee043bd7c2cc3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4d6a170d4c695b90040",
+    "url": "./static/css/main.86e637f9.chunk.css"
+  },
+  {
+    "revision": "0065ac3b6e27ca37dab3",
+    "url": "./static/js/2.723a1918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.723a1918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d6a170d4c695b90040",
+    "url": "./static/js/main.a7a3c4c1.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
